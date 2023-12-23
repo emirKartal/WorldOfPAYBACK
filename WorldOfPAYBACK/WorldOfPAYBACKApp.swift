4185c701895ba10c7eddfdb19da69a8b24a43cea
Emir Kartal <emir.kartal@apollon-fulfillment.com>
2023-12-23 20:17:08 +0100

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by emir kartal on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
